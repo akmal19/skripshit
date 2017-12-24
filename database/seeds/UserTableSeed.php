e46159bafd79e9dd2254a32920611efb7e8f46c6
Akmal <rigid1906@gmail.com>
2017-12-24 13:08:33 +0700

    add repo login register

---
+++ database/seeds/UserTableSeed.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UserTableSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+		User::create(array(
+			'name'     => 'Admiinistrator',
+			'email'    => 'admin@skripsi',
+			'password' => Hash::make('admin'),
+		));
+    }
+}
